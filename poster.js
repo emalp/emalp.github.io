3b0207c25ddd1f414ada7f89cb4fa32f06361798
emalp <emalppro@gmail.com>
2020-09-17 22:43:52 +1000

    now

--- poster.js
+++ poster.js
@@ -1,9 +1,13 @@
 var data = {
 'user': 'test',
 'id': 0
-}
+};
 
-fetch('http://ptl-74c77e05-9175f689.libcurl.so/share', {
+var stringed = JSON.stringify(data);
+console.log("Sending:");
+console.log(stringed);
+
+data = fetch('http://ptl-74c77e05-9175f689.libcurl.so/share', {
 	method: 'POST'.
 	headers: {
 		'Content-Type':'application/json'
