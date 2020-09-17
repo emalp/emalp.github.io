754ea3f3d8e2db05137ed7c213070f784a5037e6
emalp <emalppro@gmail.com>
2020-09-18 07:05:46 +1000

    now

--- poster.js
+++ poster.js
@@ -10,7 +10,7 @@ console.log(stringed);
 var result = fetch('http://ptl-74c77e05-9175f689.libcurl.so/share', {
 		method: 'POST',
 		headers: {
-			'Content-Type':'application/json'
+			'Content-Type':'text/plain'
 		},
 		redirect:'follow',
 		body: JSON.stringify(data)
